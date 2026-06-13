9abdbd14a7c8b35a9a5b7ec3dc676b3877bc7b01
Seva Zakharchenko <vseva80@yandex.ru>
2026-06-13 07:10:20 -0600

    ++ StateMachine for aggregates

--- CafeApp.Domain/Class1.cs
+++ CafeApp.Domain/Class1.cs
@@ -22,6 +22,18 @@ public class TableSession
     public int PartySize { get; set; }
 }
 
+public class TableSeessionStatus
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+public enum TableSessionStatusEnum
+{
+    Opened = 1,
+    Active = 2,
+    Closed = 3
+}
+
 public class  Staff
 {
     public int Id { get; set; }
@@ -36,9 +48,24 @@ public class Booking
     public int Id { get; set; }
     public int TableId { get; set; } //FK
     public int CustomerId { get; set; } //FK
+    public int BookingStatusId { get; set; } //FK
     public DateTime ReservationTime { get; set; }
     public int PeopleNumber { get; set; }
 }
+public class BookingStatus
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+public enum BookingStatusEnum
+{
+    Canceled = 1,
+    Confirmed = 2,
+    Seated = 3,
+    Completed = 4
+}
+
+
 public class Customer
 {
     public int Id { get; set; }
@@ -49,7 +76,7 @@ public class Customer
 
 
 
-public class Order
+public class Orders
 {
     public int Id { get; set; }
     public int OrderStatusId { get; set; } //FK
@@ -63,11 +90,25 @@ public class OrderStatus
     public int Id { get; set; }
     public string Name { get; set; }
 }
+public enum OrderStatusEnum
+{
+    Created = 1,
+    Confirmed = 2,
+    Pending = 3,
+    Ready = 4,
+    Closed = 5
+}
 
 public class OrderType
 {
     public int Id { get; set; }
     public string Name { get; set; }
+}
+public enum OrderTypeEnum
+{
+    DineIn = 1,
+    TakeAway = 2,
+    Delivery = 3
 }
 
 public class  OrderItem
@@ -105,13 +146,24 @@ public class PayType
     public int Id { get; set; }
     public string Name { get; set; }
 }
+public enum PayTypeEnum
+{
+    Cash = 1,
+    CreditCard = 2,
+    MobilePayment = 3
+}
 
 public class PayStatus
 {
     public int Id { get; set; }
     public string Name { get; set; }
 }
-
+public enum PayStatusEnum
+{
+    Pending = 1,
+    Completed = 2,
+    Failed = 3
+}
 
 
 public class Bill
@@ -128,6 +180,13 @@ public class BillStatus
 {
     public int Id { get; set; }
     public string Name { get; set; }
+}
+public enum BillStatusEnum
+{
+    Draft = 1,
+    Issued = 2,
+    Paid = 3,
+    Refunded = 4
 }
 
 public class BillAdjustment
